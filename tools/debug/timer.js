57fe23fbb44d6eba3102a0131ccc8bc22edcb997
angus croll <angus@twitter.com>
2013-03-06 14:11:19 -0800

    added threshold

--- tools/debug/timer.js
+++ tools/debug/timer.js
@@ -8,11 +8,16 @@ define(
           var t1 = timer('merge options');
           //merge options code
           t1(); //'merge options 232 ms'
+
+          var t1 = timer('merge options', 300);
+          //merge options code
+          t1(); //[nothing to report]
     */
-    return function(name) {
+    return function(name, threshold) {
       var startAt = Date.now();
       return function() {
-        console.log(name, Date.now() - startAt, 'ms');
+        var duration = Date.now() - startAt;
+        (threshold || 0) <= duration && console.log(name, Date.now() - startAt, 'ms');
       }
     }
   }
