5989c940cf9e358eead9da341aac152135618221
Nicolas Gallagher <nicolasgallagher@gmail.com>
2013-07-16 08:26:11 -0700

    Simplify Karma configuration
    Remove configuration that merely repeats the default settings.

--- karma.conf.js
+++ karma.conf.js
@@ -43,28 +43,11 @@ reporters = [
   'dots'
 ];
 
-// web server port
-// CLI --port 9876
-port = 9876;
-
-// cli runner port
-// CLI --runner-port 9100
-runnerPort = 9100;
-
-// enable / disable colors in the output (reporters and logs)
-// CLI --colors --no-colors
-colors = true;
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-// CLI --log-level debug
-logLevel = LOG_INFO;
-
 // enable / disable watching file and executing tests whenever any file changes
 // CLI --auto-watch --no-auto-watch
 autoWatch = true;
 
-// Start these browsers, currently available:
+// start these browsers, currently available:
 // - Chrome
 // - ChromeCanary
 // - Firefox
@@ -78,14 +61,6 @@ browsers = [
   'Firefox'
 ];
 
-// If browser does not capture in given timeout [ms], kill it
-// CLI --capture-timeout 5000
-captureTimeout = 5000;
-
-// Auto run tests on start (when browsers are captured) and exit
+// auto run tests on start (when browsers are captured) and exit
 // CLI --single-run --no-single-run
 singleRun = false;
-
-// report which specs are slower than 500ms
-// CLI --report-slower-than 500
-reportSlowerThan = 500;
