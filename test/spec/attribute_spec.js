4e62455ade4f12039c53744133df6387e447bc50
Dan Webb <dan@twitter.com>
2014-04-07 13:38:46 -0700

    added full suite for this.defaultAttrs

--- test/spec/attribute_spec.js
+++ test/spec/attribute_spec.js
@@ -86,4 +86,76 @@ define(['lib/component'], function (defineComponent) {
     });
   });
 
+  describe('(Core) this.defaultAttrs', function() {
+    function testComponentDefaultAttrs() {
+      this.defaultAttrs({core: 35});
+    }
+
+    function testComponentDefaultAttrsRequired() {
+      this.defaultAttrs({req: null});
+    }
+
+    function withGoodDefaults() {
+      this.defaultAttrs({extra: 38});
+    }
+
+    function withBadDefaults() {
+      this.defaultAttrs({core: 38});
+    }
+
+    it('adds core defaults', function () {
+      var TestComponent = defineComponent(testComponentDefaultAttrs);
+      var instance = (new TestComponent).initialize(document.body);
+
+      expect(instance.attr.core).toBe(35);
+
+      TestComponent.teardownAll();
+    });
+
+    it('throws error if required attr not specified', function () {
+      var TestComponent = defineComponent(testComponentDefaultAttrsRequired);
+      expect(function () {
+        var instance = (new TestComponent).initialize(document.body);
+      }).toThrow('Required attribute "req" not specified in attachTo for component "testComponentDefaultAttrsRequired".');
+
+      TestComponent.teardownAll();
+    });
+
+    it("doesn't throw error if required attr is specified", function () {
+      var TestComponent = defineComponent(testComponentDefaultAttrsRequired);
+      expect(function () {
+        var instance = (new TestComponent).initialize(document.body, { req: 'hello' });
+      }).not.toThrow();
+
+      TestComponent.teardownAll();
+    });
+
+    it("doesn't attach to empty jQuery objects", function () {
+      var TestComponent = defineComponent(testComponentDefaultAttrs);
+      var trouble = function () {
+        (new TestComponent).initialize();
+      };
+      expect(trouble).toThrow(new Error("Component needs a node"));
+    });
+
+    it('adds mixin defaults', function () {
+      var TestComponent = defineComponent(testComponentDefaultAttrs, withGoodDefaults);
+      var instance = (new TestComponent).initialize(document.body);
+
+      expect(instance.attr.extra).toBe(38);
+
+      TestComponent.teardownAll();
+    });
+
+    it('adds core and mixin defaults', function () {
+      var TestComponent = defineComponent(testComponentDefaultAttrs, withGoodDefaults);
+      var instance = (new TestComponent).initialize(document.body);
+      expect(instance.attr.core).toBe(35);
+      expect(instance.attr.extra).toBe(38);
+
+      TestComponent.teardownAll();
+    });
+
+  });
+
 });
