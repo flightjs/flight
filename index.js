e66b0ec7d644a566b9aca31ec10469d64a596eda
Mike Fulcher <mike@drawingablank.me>
2014-07-31 22:34:18 -0700

    Add the debug module to the entry file

--- index.js
+++ index.js
@@ -6,18 +6,20 @@ define(
     './lib/advice',
     './lib/component',
     './lib/compose',
+    './lib/debug',
     './lib/logger',
     './lib/registry',
     './lib/utils'
   ],
 
-  function(advice, component, compose, logger, registry, utils) {
+  function(advice, component, compose, debug, logger, registry, utils) {
     'use strict';
 
     return {
       advice: advice,
       component: component,
       compose: compose,
+      debug: debug,
       logger: logger,
       registry: registry,
       utils: utils
